096b180139aca48acff90893202f712b97867689
shokh0126mardonov <smardonov203@gmail.com>
2025-10-03 00:39:38 +0500

    init

---
+++ app/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import URL,create_engine
+from config import setting
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+url_object = URL.create(
+    'postgresql+psycopg2',
+    username = setting.DB_USER,
+    password = setting.DB_PASSWORD,
+    port = setting.DB_PORT,
+    host = setting.DB_HOST,
+    database = setting.DB_NAME 
+)
+
+engine = create_engine(url_object)
+
+LocalSesion = sessionmaker(bind=engine)
+
+Base = declarative_base()
